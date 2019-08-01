6aa83e9c0a661b01b021858facf812184281b868
Stanly Rodriguez <ibestanly@gmail.com>
2019-08-01 10:16:09 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,42 @@
+// const person = {
+//   results: [
+//     {
+//       gender: "male",
+//       name: { title: "mr", first: "thomas", last: "gauthier" },
+//       location: {
+//         street: "966 rue des abbesses",
+//         city: "villeurbanne",
+//         state: "vosges",
+//         postcode: 87317,
+//         coordinates: { latitude: "3.4738", longitude: "-82.8250" },
+//         timezone: {
+//           offset: "+5:30",
+//           description: "Bombay, Calcutta, Madras, New Delhi"
+//         }
+//       },
+//       email: "thomas.gauthier@example.com",
+//       login: {
+//         uuid: "e5b1affd-123e-4d37-9021-31fcd15c13e3",
+//         username: "blackbutterfly278",
+//         password: "666666",
+//         salt: "fkm3YnBM",
+//         md5: "fc7d2b9fa9a8d2b7e7ac4ec08eadcb97",
+//         sha1: "00371365d26581bd4371804f2573a979bd4a0a50",
+//         sha256:
+//           "4a8fca680e2c8684b76db6ea1a12487190b642062a3c02255c4341d7e68c2973"
+//       },
+//       dob: { date: "1950-04-03T04:27:25Z", age: 69 },
+//       registered: { date: "2017-10-08T19:18:59Z", age: 1 },
+//       phone: "05-13-80-28-23",
+//       cell: "06-28-61-70-13",
+//       id: { name: "INSEE", value: "1NNaN96389942 74" },
+//       picture: {
+//         large: "https://randomuser.me/api/portraits/men/76.jpg",
+//         medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+//         thumbnail: "https://randomuser.me/api/portraits/thumb/men/76.jpg"
+//       },
+//       nat: "FR"
+//     }
+//   ],
+//   info: { seed: "f31915994f440349", results: 1, page: 1, version: "1.2" }
+// };
